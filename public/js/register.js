afa61a1308cb1882e3c8cdad83343fba766ed70c
Tim <95177691+tmazitov@users.noreply.github.com>
2021-12-12 21:48:32 +0300

    add username and password length criterion

--- public/js/register.js
+++ public/js/register.js
@@ -1,34 +1,45 @@
-function getRndInteger(min, max) {
-    return Math.floor(Math.random() * (max - min + 1)) + min;
-}
-
 function addUser() {
 
-    let name = document.getElementById("username").value
-    let pass = document.getElementById("password").value
+    // Block for error message
+    let errBlock = document.getElementById("register_message")
 
+    // Get username and check it
+    let name = document.getElementById("username").value
+    if (name.length < 4) {
+        errBlock.innerHTML = "Username is too short"
+        return
+    }
+
+    // Get username and check it
+    let pass = document.getElementById("password").value
+    if (pass.length < 8) {
+        errBlock.innerHTML = "Password is too short"
+        return
+    }
+
+    // Init request
     var xhr = new XMLHttpRequest();
 
+    // Set params
     var params =
         'username=' + encodeURIComponent(name) +
         '&password=' + encodeURIComponent(pass);
 
+    // Request configuration
     xhr.open("POST", '/user/new?' + params, true);
 
-    xhr.onreadystatechange = function(oEvent) {
+    // Error handler
+    xhr.onreadystatechange = function() {
         if (xhr.readyState === 4) {
             console.log(xhr.status + ": " + xhr.statusText)
             if (xhr.status === 200) {
                 console.log(xhr.responseText)
             } else if (xhr.status == 403) {
-                let block = document.getElementById("register_message")
-                block.innerHTML += "User with this username alredy exists"
+                errBlock.innerHTML = "User with this username alredy exists"
             }
         }
     }
 
+    // Send request
     xhr.send();
-
-
-
 }
