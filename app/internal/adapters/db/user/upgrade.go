0371d60ec3a8170fd58822d8512b94bc22f9fc05
Tim <95177691+tmazitov@users.noreply.github.com>
2022-01-04 18:36:35 +0300

    debug upgrade of role account

--- app/internal/adapters/db/user/upgrade.go
+++ app/internal/adapters/db/user/upgrade.go
@@ -10,9 +10,9 @@ func (us *userStorage) UpgradeRole(user *models.User) error {
 	var err error
 
 	if user.Role == "User" {
-		_, err = us.Conn.Exec("update users set role=$1 where role=$2 username=$3", "Moder", user.Role, user.Username)
+		_, err = us.Conn.Exec("update users set role=$1 where username=$2", "Moder", user.Username)
 	} else if user.Role == "Moder" {
-		_, err = us.Conn.Exec("update users set role=$1 where role=$2 username=$3", "Admin", user.Role, user.Username)
+		_, err = us.Conn.Exec("update users set role=$1 where username=$2", "Admin", user.Username)
 	} else {
 		return nil
 	}
