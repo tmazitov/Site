8e622f7fa285c6e4cd843afdfbe50fc1aa284bf6
Tim <95177691+tmazitov@users.noreply.github.com>
2022-01-11 18:25:38 +0300

    Update client.js

--- web/src/client/client.js
+++ web/src/client/client.js
@@ -1,8 +1,18 @@
 import axios from 'axios'
+import { readValue } from '../actions/jwt';
 
 const client = axios.create({
-    baseURL: 'http://localhost:8000/api/',
+    baseURL: process.env.VUE_APP_API_PATH,
+    headers: {
+      "Authorization": "Bearer "+ readValue(),
+    },
+    withCredentials: true 
   });
 
+const auth = axios.create({
+  baseURL: process.env.VUE_APP_API_PATH,
+  withCredentials: true
+})
 
+export {auth}
 export default client
