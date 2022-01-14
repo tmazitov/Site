1ca9ac2581ca261fb64b59af5d5c41cb47d23bd6
Tim <95177691+tmazitov@users.noreply.github.com>
2022-01-14 21:00:59 +0300

    update order model

--- app/internal/domain/models/order.go
+++ app/internal/domain/models/order.go
@@ -1,11 +1,13 @@
 package models
 
 type Order struct {
+	UUID        string
 	Title       string
+	Writer      string
 	Date        int64
 	HourCount   int
 	Status      string
 	FromAddress string
-	ToAddress   []string
+	ToAddress   string
 	Comment     string
 }
