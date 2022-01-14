275b9d03cb4abb3e8138613f4d3b9e0aa46272e8
Tim <95177691+tmazitov@users.noreply.github.com>
2022-01-14 20:58:26 +0300

    add delete handler

--- app/internal/adapters/api/order/delete.go
+++ app/internal/adapters/api/order/delete.go
@@ -1,11 +1,53 @@
 package order
 
 import (
+	"encoding/json"
+	"io/ioutil"
+	"log"
 	"net/http"
 
 	"github.com/julienschmidt/httprouter"
 )
 
+type deleteParams struct {
+	UUID string
+}
+
 func (h *handler) Delete(w http.ResponseWriter, r *http.Request, ps httprouter.Params) {
+
+	// Get username from access token
+	_, role, err := h.JWTHelper.GetUserByToken(r)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+
+	if role != "Admin" {
+		log.Println(err.Error())
+		http.Error(w, "Forbidden", http.StatusForbidden)
+		return
+	}
+
+	var params deleteParams
+	body, err := ioutil.ReadAll(r.Body)
+	if err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+	if err = json.Unmarshal(body, &params); err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+
+	if err := h.orderService.Delete(params.UUID); err != nil {
+		log.Println(err.Error())
+		http.Error(w, "Internal Server Error", 500)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
 
 }
